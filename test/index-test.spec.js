1d883694be42cfb2f1138b8baebf221f6cf377fd
SarAbigail <samihimezu@gmail.com>
2020-02-03 14:36:00 -0500

    agrega funcion archivo .md actualizado

--- test/index-test.spec.js
+++ test/index-test.spec.js
@@ -1,36 +1,36 @@
 
-const route = require('../src/index.js');
+const functions = require('../src/index.js');
 
 describe('.pathAbsolute', () => {
   it('retorna true si la ruta es absoluta', () => {
-    expect(route.pathAbsolute('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md')).toBe(true);
+    expect(functions.pathAbsolute('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md')).toBe(true);
   });
 
   it('retorna false si la ruta relativa', () => {
-    expect(route.pathAbsolute('./README.md')).toBe(false);
+    expect(functions.pathAbsolute('./README.md')).toBe(false);
   });
 });
 
 describe('resolveToAbsolute', () => {
   it('resuelve una ruta relativa a absoluta', () => {
-    expect(route.resolveToAbsolute('./README.md')).toBe('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md');
+    expect(functions.resolveToAbsolute('./README.md')).toBe('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md');
   });
 });
 
 describe('identifyFile', () => {
   it('identifica si la ruta recibida es un archivo', () => {
-    expect(route.identifyFile('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md')).toBe(true);
+    expect(functions.identifyFile('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md')).toBe(true);
   });
 });
 
 describe('thisIsMd', () => {
   it('Debería retornar function', () => {
-    expect(typeof route.thisIsMd).toBe('function');
+    expect(typeof functions.thisIsMd).toBe('function');
   });
   it('Debería retornar false si el file no tiene extensión MD', () => {
-    expect(route.thisIsMd('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/package.json')).toBe(false);
+    expect(functions.thisIsMd('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/package.json')).toBe(false);
   });
   it('Debería retornar true si el file tiene extensión MD', () => {
-    expect(route.thisIsMd('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md')).toBe(true);
+    expect(functions.thisIsMd('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md')).toBe(true);
   });
 });
