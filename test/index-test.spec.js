9eac04c7edc4a51e310fc23adf50a1fd03887a87
SarAbigail <samihimezu@gmail.com>
2020-02-03 14:48:47 -0500

    actualiza funcion archivo .md

--- test/index-test.spec.js
+++ test/index-test.spec.js
@@ -2,6 +2,9 @@
 const functions = require('../src/index.js');
 
 describe('.pathAbsolute', () => {
+  it('Debería retornar function', () => {
+    expect(typeof functions.pathAbsolute).toBe('function');
+  });
   it('retorna true si la ruta es absoluta', () => {
     expect(functions.pathAbsolute('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md')).toBe(true);
   });
@@ -12,14 +15,23 @@ describe('.pathAbsolute', () => {
 });
 
 describe('resolveToAbsolute', () => {
+  it('Debería retornar function', () => {
+    expect(typeof functions.resolveToAbsolute).toBe('function');
+  });
   it('resuelve una ruta relativa a absoluta', () => {
     expect(functions.resolveToAbsolute('./README.md')).toBe('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md');
   });
 });
 
 describe('identifyFile', () => {
+  it('Debería retornar function', () => {
+    expect(typeof functions.identifyFile).toBe('function');
+  });
   it('identifica si la ruta recibida es un archivo', () => {
     expect(functions.identifyFile('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/README.md')).toBe(true);
+  });
+  it('identifica si la ruta recibida no es un archivo', () => {
+    expect(functions.identifyFile('/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links')).toBe(false);
   });
 });
 
