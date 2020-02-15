136dfce1c95c79cc4cdeb59441f5a8d8100c538a
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-02-14 19:07:54 -0500

    inicia stats

--- test/index-test.spec.js
+++ test/index-test.spec.js
@@ -1,6 +1,7 @@
 const fetchMock = require('fetch-mock');
 const functions = require('../src/index.js');
 const validate = require('../src/validate.js');
+const stats = require('../src/stats.js');
 
 fetchMock.mock('*', 200);
 
@@ -133,5 +134,56 @@ describe('validateLink', () => {
         expect(response).toStrictEqual(output);
         done();
       });
+  });
+});
+
+const array = [
+  {
+    href: 'https://nodejs.org/es/',
+    path: '/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/test/pruebas/node.md',
+    status: 200,
+    statusText: 'OK',
+    text: 'Node.js',
+  },
+  {
+    href: 'https://nodejs.org/es/.co',
+    path: '/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/test/pruebas/node.md',
+    status: 200,
+    statusText: 'OK',
+    text: 'Node.js',
+  },
+  {
+    href: 'https://nodefdjs.org/es/',
+    path: '/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/test/pruebas/node.md',
+    text: 'Noesuunlink.js',
+  },
+
+  {
+    href: 'https://noesunlink.or/es/',
+    path: '/home/laboratoria/Proyecto nuevo/LIM011-fe-md-links/test/pruebas/node.md',
+    text: 'Noesuunlink.js',
+  },
+
+];
+
+describe('Broken links', () => {
+  it('Debería retornar function', (done) => {
+    expect(typeof stats.brokenLinks).toBe('function');
+    done();
+  });
+  it('Debería retornar la cantidad de links rotos', (done) => {
+    expect(stats.brokenLinks(array)).toBe(3);
+    done();
+  });
+});
+
+describe('Broken links', () => {
+  it('Debería retornar function', (done) => {
+    expect(typeof stats.uniquesLinks).toBe('function');
+    done();
+  });
+  it('Debería retornar la cantidad de links unicos', (done) => {
+    expect(stats.uniquesLinks(array)).toBe(3);
+    done();
   });
 });
